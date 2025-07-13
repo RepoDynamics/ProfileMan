565a59b5c84a4803bcf36e6d8331dd59a0668436
Armin Ariamajd <80158628+AAriam@users.noreply.github.com>
2025-07-13 10:06:38 +0200

    Update function.py

--- pkg/src/profileit/function.py
+++ pkg/src/profileit/function.py
@@ -10,8 +10,8 @@ from matplotlib import pyplot as plt
 InputArgs = Sequence[Any]
 InputKwargs = dict[str, Any]
 Output = Any
-ArgSize = int
-ArgGen = Callable[[ArgSize], tuple[InputArgs, InputKwargs]]
+InputSize = int
+ArgGen = Callable[[InputSize], tuple[InputArgs, InputKwargs]]
 
 
 class FunctionProfiler:
@@ -19,17 +19,37 @@ class FunctionProfiler:
 
     Parameters
     ----------
-    funcs
+    function
         Function(s) to be profiled.
-    arg_gens
-        Argument generators for each function in `funcs`.
-        Each generator receives a sequence of numbers
-        indicating different input sizes
-        for the respective function's arguments,
-        and yields input arguments for that function in form of a tuple.
-        That is, each function `f` in `funcs` should be callable
-        with the corresponding generator `g` in `arg_gens`,
-        for example as `[f(*args) for args in g([1, 10, 100])]`.
+    input_gen
+        Input argument generator for each function in `function`.
+        Each generator must accept an integer as input,
+        indicating the size of the input(s) to generate.
+        The generator should return a tuple of two elements:
+        - A sequence of positional arguments for the function.
+        - A dictionary of keyword arguments for the function.
+
+        Therefore, each function `f` in `function` should be callable
+        with the corresponding generator `g` in `input_gen` as follows:
+        ```python
+        args, kwargs = g(input_size)
+        f(*args, **kwargs)
+        ```
+    function_name
+        Name(s) of the function(s) to be profiled.
+        If not provided, the function's module and qualified name will be used.
+        If the same function is provided multiple times,
+        the name will be suffixed with an index to ensure uniqueness.
+    output_evaluator
+        Optional callable that evaluates the outputs of the functions.
+        This can be used to validate or process the outputs in parallel to profiling.
+        If not provided, no output evaluation will be performed.
+        The evaluator function should accept
+        the following parameters (passed as positional arguments in this order):
+        - `input_size`: The input size that was used.
+        - `input_args`: A sequence of positional arguments used for each function.
+        - `input_kwargs`: A sequence of keyword arguments used for each function.
+        - `outputs`: A sequence of outputs returned by each function.
     """
 
     def __init__(
@@ -37,7 +57,7 @@ class FunctionProfiler:
         function: Callable | Sequence[Callable],
         input_gen: ArgGen | Sequence[ArgGen],
         function_name: Sequence[str] | None = None,
-        output_evaluator: Callable[[ArgSize, Sequence[InputArgs], Sequence[InputKwargs], Sequence[Output]], None] | None = None,
+        output_evaluator: Callable[[InputSize, Sequence[InputArgs], Sequence[InputKwargs], Sequence[Output]], None] | None = None,
     ):
         self._funcs: list[Callable] = function if isinstance(function, Sequence) else [function]
         self._input_gens: list[ArgGen] = input_gen if isinstance(input_gen, Sequence) else [input_gen]
@@ -62,7 +82,7 @@ class FunctionProfiler:
         self._func_names: list[str] = function_name
         self._output_eval = output_evaluator
 
-        self._arg_sizes: list[int] = None
+        self._input_sizes: list[int] = None
         self._runs: int = None
         self._loops_per_run: int = None
         self._results: np.ndarray = None
@@ -70,75 +90,40 @@ class FunctionProfiler:
 
     def profile(
         self,
-        arg_sizes: Sequence[int],
+        input_sizes: Sequence[InputSize],
         runs: int = 100,
         loops_per_run: int = 1,
-    ) -> None:
+    ) -> np.ndarray:
         """Profile all functions.
 
         Parameters
         ----------
-        arg_sizes
-            Different argument sizes to profile the functions with.
+        input_sizes
+            Different input sizes to profile the functions with.
         runs
-            Number of times the profiling is repeated for each argument size.
+            Number of times the profiling is repeated for each input size.
             Higher values provide more accurate results.
             The shortest duration between all runs will be selected,
             as it represents the most accurate duration.
         loops_per_run
             Number of times each run is repeated.
             For each run, the average of all loops will be selected.
+
+        Returns
+        -------
+        A 2D array with the shape `(n_functions, n_input_sizes)`,
+        containing the lowest time taken for each function at each input size.
         """
-        self._arg_sizes = arg_sizes
+        self._input_sizes = input_sizes
         self._runs = runs
         self._loops_per_run = loops_per_run
-        results = []
-        for func, arg_gen in zip(self._funcs, self._input_gens, strict=True):
-            shortest_runtime_per_argsize_for_func: list[list[float]] = []
-            for args, kwargs in arg_gen(arg_sizes):
-                all_loop_times = timeit.repeat(
-                    lambda: func(*args, **kwargs),
-                    repeat=self._runs,
-                    number=self._loops_per_run
-                )
-                shortest_loop_time = np.min(all_loop_times)
-                shortest_run_time = shortest_loop_time / self._loops_per_run
-                shortest_runtime_per_argsize_for_func.append(shortest_run_time)
-            results.append(shortest_runtime_per_argsize_for_func)
-        self._results = np.array(results)
-        return
-
-    def profile(
-        self,
-        arg_sizes: Sequence[int],
-        runs: int = 100,
-        loops_per_run: int = 1,
-    ) -> None:
-        """Profile all functions.
-
-        Parameters
-        ----------
-        arg_sizes
-            Different argument sizes to profile the functions with.
-        runs
-            Number of times the profiling is repeated for each argument size.
-            Higher values provide more accurate results.
-            The shortest duration between all runs will be selected,
-            as it represents the most accurate duration.
-        loops_per_run
-            Number of times each run is repeated.
-            For each run, the average of all loops will be selected.
-        """
-        self._arg_sizes = arg_sizes
-        self._runs = runs
-        self._loops_per_run = loops_per_run
-        self._results = np.empty((len(self._funcs), len(arg_sizes)), dtype=float)
-        for arg_size_idx, arg_size in enumerate(arg_sizes):
+        self._results = np.empty((len(self._funcs), len(input_sizes)), dtype=float)
+        for input_size_idx, input_size in enumerate(input_sizes):
             input_args = []
             input_kwargs = []
             outputs = []
             for func_idx, (func, arg_gen) in enumerate(zip(self._funcs, self._input_gens, strict=True)):
-                args, kwargs = arg_gen(arg_size)
+                args, kwargs = arg_gen(input_size)
                 if self._output_eval is not None:
                     input_args.append(args)
                     input_kwargs.append(kwargs)
@@ -150,12 +135,27 @@ class FunctionProfiler:
                 )
                 shortest_loop_time = np.min(all_loop_times)
                 shortest_run_time = shortest_loop_time / self._loops_per_run
-                self._results[func_idx, arg_size_idx] = shortest_run_time
+                self._results[func_idx, input_size_idx] = shortest_run_time
             if self._output_eval is not None:
-                self._output_eval(input_args, input_kwargs, outputs)
+                self._output_eval(input_size, input_args, input_kwargs, outputs)
         return self._results
 
-    def plot(self, show: bool = True):
+    def plot(self, show: bool = True) -> tuple[plt.Figure, plt.Axes]:
+        """Plot the profiling results.
+
+        This function must be called after `profile` has been executed.
+        It plots the latest profiling results for each function
+        against the input sizes used during profiling.
+
+        Parameters
+        ----------
+        show
+            Call `plt.show()` after plotting.
+
+        Returns
+        -------
+        A tuple containing the figure and axes of the plot.
+        """
         if self._results is None:
             raise ValueError(
                 "No profiling has been performed yet; call `FunctionProfiler.profile` first."
@@ -164,7 +164,7 @@ class FunctionProfiler:
         artists = []
         for func_name, result in zip(self._func_names, self._results, strict=True):
             (line,) = ax.plot(
-                self._arg_sizes,
+                self._input_sizes,
                 result,
                 marker=".",
                 label=func_name,
